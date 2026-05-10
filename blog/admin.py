941411039b1dfacaf1d656af534e898ad701fadf
kutay000 <kutaybyb70@gmail.com>
2026-05-10 17:51:54 +0300

    final logout fix

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from .models import Post, Comment
 
-admin.site.register(Post)
-admin.site.register(Comment)
+
+# 📄 POST ADMIN
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author')
+    search_fields = ('title', 'content')
+    list_filter = ('author',)
+
+
+# 💬 COMMENT ADMIN
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'author', 'created_at')
+    search_fields = ('content',)
+    list_filter = ('author',)
