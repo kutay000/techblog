73398a189106f613bd072b1c23513afba575a0bb
kutay000 <kutaybyb70@gmail.com>
2026-05-10 17:22:55 +0300

    fix forms import error

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,14 @@
-from .models import Comment
 from django import forms
+from .models import Post, Comment
 
+# 📝 POST FORM
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content']
+
+
+# 💬 COMMENT FORM
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
