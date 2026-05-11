340de97dc996ba915f44bb437ebc33da0fbb43ae
kutay000 <kutaybyb70@gmail.com>
2026-05-11 12:28:22 +0300

    fix postgres connection

--- config/settings.py
+++ config/settings.py
@@ -61,11 +61,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # 🗄️ DATABASE
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        "postgresql://postgres:techblog456852@db.jhtxuhtyordcgpgkqvgt.supabase.co:5432/postgres",
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
